8aa0a13266755e617766315e2f1bdc859da5f72e
niiiiiiiiiiix <nixtwy@gmail.com>
2021-06-12 18:42:41 +0800

    Update addAuthor in mutation

--- server.js
+++ server.js
@@ -118,6 +118,18 @@ const RootMutationType = new GraphQLObjectType({
         return book;
       },
     },
+    addAuthor: {
+      type: AuthorType,
+      description: "Add an author",
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parent, args) => {
+        const author = { id: authors.length + 1, name: args.name };
+        authors.push(author);
+        return author;
+      },
+    },
   }),
 });
 
